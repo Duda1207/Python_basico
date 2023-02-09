897f9dc4b5f0a767b8c0cadb70e4c42319678329
Duda1207 <124939346+Duda1207@users.noreply.github.com>
2023-02-09 19:59:03 -0300

    Add files via upload

---
+++ Python/duda.py
@@ -0,0 +1,4 @@
+Conjuge1= ("Maria Eduarda")
+Casou= (" se casou com")
+Conjuge2= (" Raylison Borges")
+print (Conjuge1+ Casou+ Conjuge2)
---
+++ Python/tarefa 2.py
@@ -0,0 +1,4 @@
+Base= int (input(" Digite o valor da Base: "))
+Altura= int (input(" Digite o valor da Altura: "))
+Área= Base*Altura4
+print (f" A Área do triângulo é {Área}")
